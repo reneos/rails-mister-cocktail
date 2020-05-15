aae5c27c2145c92c270111bf9fbe1b4d151d81db
Nicole Wong <nrewong@gmail.com>
2020-05-15 16:07:48 +0900

    mobile styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
     @review.cocktail = @cocktail
+    @dose = Dose.new # needed in case there's an error and the page is re-rendered
     if @review.save
       redirect_to cocktail_path(@cocktail)
     else
